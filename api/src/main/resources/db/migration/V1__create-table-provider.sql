6630e36d3164a017809528038dc59fc1638cb20d
davifariasp <davi.informatica8@gmail.com>
2024-06-03 11:05:26 -0300

    reconfigurando bd

--- api/src/main/resources/db/migration/V1__create-table-provider.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE provider (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    contact VARCHAR(255) NOT NULL,
-    type VARCHAR(50) NOT NULL,
-    additional_info VARCHAR(255)
-);
--- api/src/main/resources/db/migration/V1__start-bd.sql
+++ api/src/main/resources/db/migration/V1__start-bd.sql
@@ -1,3 +1,11 @@
+CREATE TABLE provider (
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    contact VARCHAR(255) NOT NULL,
+    type VARCHAR(50) NOT NULL,
+    additional_info VARCHAR(255)
+);
+
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
